02f987cdfdb4ae89c3c0bfee67557fa9cff10381
Juan Vásquez <jlemus003@gmail.com>
2016-07-30 19:12:52 -0600

    Se inicio a programar el analizador de los comando de consola
    Falta como 2 estructs todavia proximos a agregar y se continuara el
    analizador de los comandos para el proeycto

--- proyecto1/main.c
+++ proyecto1/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 typedef struct particion{
     char part_status;
@@ -28,9 +33,83 @@ typedef struct EBR{
     int part_next;
     char part_name[16];
 }EBR;
-
+void analisador();
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+   char str[80] = "This is  www.tutorialspoint.com - website";
+   const char s[2] = " ";
+   char *token;
+
+   /* get the first token */
+   token = strtok(str, s);
+
+   /* walk through other tokens */
+   /*while( token != NULL )
+   {
+      printf( " %s\n", token );
+
+      token = strtok(NULL, s);
+   } */
+
+    /*char pala[300];
+    char *p = "puta mierda;";
+    strcpy(pala,p);
+    printf(pala);*/
+    char p[50] = "hola";
+    char *d = "hola";
+    if(strcmp(p,"ptua")==0)
+    {
+        printf("Es igual\n");
+    }
+    else
+    printf("No es igual\n");
+    analisador();
+   return 0;
 }
+
+void analisador()
+{
+    printf("Ingrese Comando: \n");
+    char comando[500];
+    fflush(stdin);
+    scanf(" %[^\n]s", &comando);
+    if(comando[strlen(comando)-1]=='\\')
+    {
+        char restocomando[500];printf("Siguieten linea: ");
+        fflush(stdin);
+        scanf(" %[^\n]s", &restocomando);
+        strncpy(comando,comando,strlen(comando)-1);
+        strcat(comando,restocomando);
+    }
+    //printf("Ingreso: %s",comando);
+    const char delimitador[2] = " ";
+    char *token;
+    token = strtok(comando, delimitador);
+    if(token == NULL)
+    {
+        printf("A ingresado mal el comando \n");
+        return;
+    }
+
+    if(strcmp(token,"mkdisk")==0)
+    {
+        token = strtok(NULL,"::");
+        char tam[20];
+        while(token!=NULL)
+        {
+            if(strcmp(token,"-size")==0)
+            {
+                token = strtok(NULL," ");
+                strcpy(tam,token);
+            }
+            else if(strcmp(token,"")==0)
+            {
+
+            }
+            token = strtok(NULL,"::");
+        }
+    }
+
+}
+
+
